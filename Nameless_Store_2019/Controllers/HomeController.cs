477ccf0bdd8c4aeafc5cd6e43295ba13f9147c3b
Crisco <eduardohsfreitas86@gmail.com>
2019-04-12 13:05:50 -0300

    Works on Cart. Thinks goes well

--- Nameless_Store_2019/Controllers/HomeController.cs
+++ Nameless_Store_2019/Controllers/HomeController.cs
@@ -15,15 +15,170 @@ namespace Nameless_Store_2019.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Let's Talk about Nameless Store - Cars And Autoparts.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact the site owner.";
 
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Brands()
+        {
+            return View();
+        }
+
+        public ActionResult Volkswagen_cars()
+        {
+            return View();
+        }
+
+        public ActionResult General_Motors_cars()
+        {
+            return View();
+        }
+
+        public ActionResult Gol_1()
+        {
+            return View();
+        }
+
+        public ActionResult Thanks_for_your_Purchase()
+        {
+            return View();
+        }
+
+        public ActionResult Passat_LSE()
+        {
+            return View();
+        }
+
+        public ActionResult Gol_GTS_92()
+        {
+            return View();
+        }
+
+        public ActionResult Golf_GTI()
+        {
+            return View();
+        }
+
+        public ActionResult Jetta_TSI()
+        {
+            return View();
+        }
+
+        public ActionResult Passat_VR6()
+        {
+            return View();
+        }
+
+        public ActionResult Chevette_DL()
+        {
+            return View();
+        }
+
+        public ActionResult Chevy_500()
+        {
+            return View();
+        }
+
+        public ActionResult Omega_Suprema()
+        {
+            return View();
+        }
+
+        public ActionResult Gol_Promo()
+        {
+            return View();
+        }
+
+        public ActionResult Omega_Promo()
+        {
+            return View();
+        }
+
+        public ActionResult Performance_Parts()
+        {
+            return View();
+        }
+
+        public ActionResult FUll_Kit_Turbo()
+        {
+            return View();
+        }
+
+        public ActionResult Kit_Turbo_C41GE_4C()
+        {
+            return View();
+        }
+
+        public ActionResult Thanks_For_purchase_Kit()
+        {
+            return View();
+        }
+
+        public ActionResult Kit_Turbo_AP_carburetor()
+        {
+            return View();
+        }
+
+        public ActionResult Kit_To_GM_Familia_II_Engine()
+        {
+            return View();
+        }
+
+        public ActionResult Kit_Turbo_AP_Intercooler()
+        {
+            return View();
+        }
+
+        public ActionResult Turbo_Kit_Universal()
+        {
+            return View();
+        }
+
+        public ActionResult Kit_Turbo_AP_Injection()
+        {
+            return View();
+        }
+
+        public ActionResult Turbo_Kit_JDM()
+        {
+            return View();
+        }
+
+        public ActionResult Performance_Parts_Page()
+        {
+            return View();
+        }
+
+        public ActionResult Mustang_Supercharger()
+        {
+            return View();
+        }
+
+        public ActionResult Supercharger_Page()
+        {
+            return View();
+        }
+
+        public ActionResult Sport_Filtre_2E()
+        {
+            return View();
+        }
+
+        public ActionResult Admission_Header()
+        {
             return View();
         }
     }
