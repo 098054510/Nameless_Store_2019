48a854b7450521090c7943a6f80fdaea1648e7f8
Crisco <eduardohsfreitas86@gmail.com>
2019-04-12 12:53:56 -0300

    Migration from Nameless_Store-rework to Nameless_Store_2019

---
+++ Nameless_Store_2019/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nameless_Store_2019.MvcApplication" Language="C#" %>
---
+++ Nameless_Store_2019/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcCart.Models
+{
+    public class Cart
+    {
+        public int ID { get; set; }
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CartDBContext : DbContext
+    {
+        public DbSet<Cart> Cartss { get; set; }
+    }
+}
