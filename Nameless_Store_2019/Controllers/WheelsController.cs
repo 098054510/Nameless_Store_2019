968dc219374a4a50e700390c6a1103e48b1df3d2
Crisco <eduardohsfreitas86@gmail.com>
2020-01-05 15:59:55 -0300

    Individuals Pages for Wheels

--- Nameless_Store_2019/Controllers/WheelsController.cs
+++ Nameless_Store_2019/Controllers/WheelsController.cs
@@ -13,5 +13,79 @@ namespace Nameless_Store_2019.Controllers
         {
             return View();
         }
+
+        public ActionResult BBS_CH()
+        {
+            return View();
+        }
+
+        public ActionResult CARBON_BERLIN()
+        {
+            return View();
+        }
+
+        public ActionResult MANHATTAN()
+        {
+            return View();
+        }
+
+        public ActionResult BBS_RS()
+        {
+            return View();
+        }
+
+        public ActionResult CARBON_CWB()
+        {
+            return View();
+        }
+
+        public ActionResult ENKEI_PDC()
+        {
+            return View();
+        }
+         public ActionResult ENKEI_RPF1_ZEUS()
+        {
+            return View();
+        }
+
+        public ActionResult KR_K66()
+        {
+            return View();
+        }
+
+        public ActionResult RACING_MAGNUM()
+        {
+            return View();
+        }
+
+        public ActionResult RACING_PRO_STAR()
+        {
+            return View();
+        }
+
+        public ActionResult ROTIFORM_CCV()
+        {
+            return View();
+        }
+
+        public ActionResult TE37_K57()
+        {
+            return View();
+        }
+
+        public ActionResult TSW_NURBURGRING()
+        {
+            return View();
+        }
+
+        public ActionResult ROTIFORM_LAS_R()
+        {
+            return View();
+        }
+
+        public ActionResult TURBOMAC()
+        {
+            return View();
+        }
     }
 }
