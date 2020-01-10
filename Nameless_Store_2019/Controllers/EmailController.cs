3d562b258dba0f654bd0795f744ef03bbea0f9c9
Crisco <eduardohsfreitas86@gmail.com>
2020-01-10 15:38:12 -0300

    Works on Checkout page

---
+++ Nameless_Store_2019/Controllers/EmailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nameless_Store_2019.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Send_Email()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nameless_Store_2019/Views/Email/Email.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace Nameless_Store_2019.Views.Email
+{
+    public class Email
+    {
+        public string email { get; set; }
+        public string Address { get; set; }
+        public int House_number { get; set; }
+        public string Street { get; set; }
+        public int Phone_number { get; set; }
+
+        public Email(string email, string Address, int House_number, string Street, int Phone_number)
+        {
+            this.email = email;
+            this.Address = Address;
+            this.House_number = House_number;
+            this.Street = Street;
+            this.Phone_number = Phone_number;
+        }
+
+
+    }
+}
